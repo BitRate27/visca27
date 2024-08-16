495a29d5caa4c2c7c15ff09387b0e35f43ab1618
Bill Gillock <bitrate27@gmail.com>
2024-08-16 15:56:33 -0500

    Test pan/tilt

--- main.cpp
+++ main.cpp
@@ -23,8 +23,8 @@ int main(int argc, char* argv[]) {
 
     // Connected successfully, now get the zoom level
 
-    short zoomLevel = 20;
-    
+    short zoomLevel = 10;
+
     result = cameraControl.setZoomLevel(zoomLevel);
     if (result != VOK) {
         std::cerr << "Failed to set zoom level. Error code: " << result << std::endl;
@@ -36,9 +36,23 @@ int main(int argc, char* argv[]) {
         std::cerr << "Failed to get zoom level. Error code: " << result << std::endl;
         return 1; // Exit with an error code
     }
+    std::cout << "Current zoomLevel: " << zoomLevel << std::endl;
 
-    // Print the zoom level to the console
-    std::cout << "Current zoom level: " << zoomLevel << std::endl;
+    visca_tuple_t pantilt = {20,30};
+    
+    result = cameraControl.setAbsolutePanTilt(pantilt);
+    if (result != VOK) {
+        std::cerr << "Failed to set pan/tilt. Error code: " << result << std::endl;
+        return 1; // Exit with an error code
+    }
+
+    result = cameraControl.getPanTilt(pantilt);
+    if (result != VOK) {
+        std::cerr << "Failed to get pan/tilt. Error code: " << result << std::endl;
+        return 1; // Exit with an error code
+    }
+    // Print the pantilt to the console
+    std::cout << "Current pan/tilt: " << pantilt.value1 << "," << pantilt.value2 << std::endl;
 
     // Optionally, disconnect the camera
     cameraControl.disconnectCamera();
--- visca27.cpp
+++ visca27.cpp
@@ -393,7 +393,6 @@ void ValueConverter::setValue(char f, int val)
 	auto it = fields.find(f);
 	if (it == fields.end()) return;
 	ValueField field = it->second;
-	char setCommand[30] = {};
 	std::stringstream ss1;
 	ss1 << std::hex << (short)val;
 	std::string valueHex = ss1.str();
